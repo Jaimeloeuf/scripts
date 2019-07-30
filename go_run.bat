f453be5b3e58ec907c3da7539737466123600899
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-07-30 16:51:24 +0800

    Added a basic run script for go programs.

---
+++ go_run.bat
@@ -0,0 +1,4 @@
+REM Run script for Go programs that watches the files.
+REM Built on nodemon, will need to change it so it can take an arguement for running the files.
+
+nodemon -x "go run example.go"
